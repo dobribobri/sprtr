69962e6b325193a3b03ec5f9619fd9d15622bce8
dobribobri <dobrix95@gmail.com>
2024-10-17 21:56:34 +0300

    base logic implemented

---
+++ core/planck.py
@@ -0,0 +1,84 @@
+from typing import Union
+import numpy as np
+from scipy.optimize import curve_fit
+
+C1 = 37418  # Вт×мкм^4/см^2
+C2 = 14388  # мкм×К
+
+
+def planck(wavelength, T):
+    return C1 * np.power(wavelength, -5) / (np.exp(C2 / (wavelength * T)) - 1)
+
+
+class BlackBody:
+    @staticmethod
+    def intensity(wavelength: Union[float, np.ndarray], T: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+        """
+        Спектральная интенсивность излучения абсолютно черного тела
+        :param wavelength: длина волны (в мкм)
+        :param T: температура (в К)
+        :return: Интенсивность (Вт/см^2/мкм)
+        """
+        return planck(wavelength, T)
+
+    @staticmethod
+    def temperature(wavelengths: np.ndarray, intensities: np.ndarray):
+        popt, _ = curve_fit(BlackBody.intensity, wavelengths, intensities, bounds=(1e1, 1e4))
+        return popt[0]
+
+
+class Body(BlackBody):
+    def __init__(self, eps: Union[float, np.ndarray, dict] = 1.):
+        """
+        :param eps: коэффициент излучения (излучательная способность)
+        """
+        self.eps = eps
+
+    def __eps(self, wavelength: Union[float, np.ndarray]):
+        if isinstance(self.eps, dict):
+            keys = np.asarray(list(self.eps.keys()))
+
+            if isinstance(wavelength, float):
+                index = np.argmin(np.abs(keys - wavelength))
+                key = keys[index]
+                return self.eps[key]
+
+            eps = []
+            for _l in wavelength:
+                index = np.argmin(np.abs(keys - _l))
+                key = keys[index]
+                eps.append(self.eps[key])
+            return np.asarray(eps)
+
+        return self.eps
+
+    def intensity(self, wavelength: Union[float, np.ndarray], T: Union[float, np.ndarray]):
+        """
+        Спектральная интенсивность излучения
+        :param wavelength: длина волны (в мкм)
+        :param T: температура (в К)
+        :return: Интенсивность (Вт/см^2/мкм)
+        """
+        return self.__eps(wavelength) * super().intensity(wavelength, T)
+
+    def temperature(self, wavelengths: np.ndarray, intensities: np.ndarray):
+        """
+        Определение температуры по интенсивностям
+        :param wavelengths: длины волн (в мкм)
+        :param intensities: интенсивности (в К)
+        :return: Интенсивность (Вт/см^2/мкм)
+        """
+        popt, _ = curve_fit(self.intensity, wavelengths, intensities, bounds=(1e1, 1e4))
+        return popt[0]
+
+
+if __name__ == '__main__':
+    import core.test
+
+    core.test.planck_001()
+    core.test.planck_002()
+    core.test.planck_003()
+    core.test.planck_004()
+    core.test.planck_005()
+    core.test.planck_006()
+    core.test.planck_007()
---
+++ core/test.py
@@ -0,0 +1,92 @@
+from typing import Union
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from core.planck import planck, BlackBody, Body
+
+
+def planck_001(eps: Union[float, np.ndarray, dict] = 1):
+    plt.figure()
+    wavelengths = np.linspace(135, 3000, 1000) / 1000  # мкм
+
+    body = Body(eps)
+
+    for _T, c in zip([4000, 5000, 6000, 7000], ['r', 'g', 'b', 'black']):
+        plt.plot(wavelengths * 1000., body.intensity(wavelengths, _T) / np.pi,
+                 c=c, ls='-', label='{} K, модель'.format(_T))
+
+    from radis import planck
+
+    for _T, c in zip([4000, 5000, 6000, 7000], ['r', 'g', 'b', 'black']):
+        plt.plot(wavelengths * 1000., planck(wavelengths * 1000., _T),
+                 c=c, ls='--', label='{} K, radis'.format(_T))
+
+    plt.xlabel(r'Длина волны $\lambda$, нм')
+    plt.ylabel(r'Интенсивность I, Вт/ср/см$^2$/мкм')
+
+    plt.legend(loc='best', frameon=False)
+    plt.show()
+
+
+def planck_002():
+    eps = {1: 1, 2: 1., 2.5: 4}
+    planck_001(eps)
+
+
+def planck_003():
+    body = Body(1)
+    lambdas = np.array([0.45, 0.6, 0.8, 1.0, 1.2, 1.38, 1.61, 1.8, 2.0, 2.15])[::-1]
+    T_range = [1000, 1500, 1773, 2000, 2500, 4000]
+    data = []
+    for n, wavelength in enumerate(lambdas):
+        intensities = [body.intensity(wavelength, T=T) for T in T_range]
+        data.append([n + 1,
+                     wavelength,
+                     *intensities])
+    data = np.asarray(data)
+    data = pd.DataFrame(data=data[:, 1:], index=data[:, 0],
+                        columns=["мкм"] + ['{} K'.format(T) for T in T_range])
+    data.to_excel('planck_003.xlsx')
+
+
+def planck_004():
+    plt.figure()
+    wl = np.linspace(0.1, 3, 100)
+    plt.plot(wl, planck(wl, 3000))
+
+    Ldata = np.asarray([0.135, 1.1, 2])
+    Idata = planck(Ldata, np.asarray([3000, 2990, 3010]))
+
+    plt.scatter(Ldata, Idata)
+
+    body = BlackBody()
+    T = body.temperature(Ldata, Idata)
+    plt.title('T = {}'.format(T))
+    plt.plot(wl, planck(wl, T))
+    plt.show()
+
+
+def planck_005(eps: Union[float, np.ndarray, dict] = 1):
+    body = Body(eps=eps)
+
+    plt.figure()
+    wl = np.linspace(0.1, 3, 100)
+    plt.plot(wl, body.intensity(wl, 3000))
+
+    Ldata = np.asarray([0.135, 1.1, 2])
+    Idata = body.intensity(Ldata, np.asarray([3000, 2990, 3010]))
+
+    plt.scatter(Ldata, Idata)
+
+    T = body.temperature(Ldata, Idata)
+    plt.title('T = {}'.format(T))
+    plt.plot(wl, body.intensity(wl, T))
+    plt.show()
+
+
+def planck_006():
+    planck_005(eps=0.5)
+
+
+def planck_007():
+    planck_005(eps={1: 1, 2: 1., 2.5: 4})
